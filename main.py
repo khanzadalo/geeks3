26d3da317bb135bbf77b6275f9afca31d8f90c8a
khanzadalo <khanzadaomorova@gmail.com>
2023-12-10 22:26:39 +0600

    lesson1

--- main.py
+++ main.py
@@ -1,16 +1,23 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+from aiogram import executor
+from config import dp
+from handlers import (
+    start,
+    questionnaire
+)
+from database import sql_commands
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+async def on_startup(_):
+    db = sql_commands.Database()
+    db.sql_create_tables()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+start.register_start_handlers(dp=dp)
+questionnaire.register_questionnaire_handlers(dp=dp)
+
+if __name__ == "__main__":
+    executor.start_polling(
+        dp,
+        skip_updates=True,
+        on_startup=on_startup
+    )
